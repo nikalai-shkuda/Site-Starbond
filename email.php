ff2ab5ca454e44cd8c2853f77389596823f499f5
nikkola07 <nikolayshkuda@mail.ru>
2019-02-05 14:30:15 +0300

    Cайт Starbond

---
+++ email.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+$recepient = "nikolayshkuda@mail.ru";
+$sitename = "Starbond.by";
+
+$name = trim($_POST["contactName"]);
+$phone = trim($_POST["contactTelephone"]);
+$text = trim($_POST["contactMessage"]);
+$message = "Имя: $name \nТелефон: $phone \nСообщение: $text";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+
+?>
